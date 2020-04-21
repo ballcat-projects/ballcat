1a41cc2b26c953f8ba464587bca03d82e7df0cd9
hccake <chengbohua@foxmail.com>
2020-04-21 10:55:50 +0800

    :art: conf配置类加一些condition条件注解

--- ballcat-common/ballcat-common-conf/src/main/java/com/hccake/ballcat/common/conf/mybatis/MybatisPlusConfig.java
+++ ballcat-common/ballcat-common-conf/src/main/java/com/hccake/ballcat/common/conf/mybatis/MybatisPlusConfig.java
@@ -1,14 +1,17 @@
 package com.hccake.ballcat.common.conf.mybatis;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * @author
- * @date 2017/10/29
+ * @author hccake
+ * @date 2020/04/19
+ * 默认配置MybatisPlus分页插件，通过conditional注解达到覆盖效用
  */
 @Configuration
+@ConditionalOnMissingBean(PaginationInterceptor.class)
 public class MybatisPlusConfig {
 
 	/**
@@ -18,6 +21,15 @@ public class MybatisPlusConfig {
 	@Bean
 	public PaginationInterceptor paginationInterceptor() {
 		return new PaginationInterceptor();
+	}
+
+	/**
+	 * 自动填充处理类
+	 * @return FillMetaObjectHandle
+	 */
+	@Bean
+	public FillMetaObjectHandle fillMetaObjectHandle() {
+		return new FillMetaObjectHandle();
 	}
 
 }
