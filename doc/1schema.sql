e4bca683ca5257aab0ff0e29358dfe1397cd8d02
hccake <chengbohua@foxmail.com>
2020-04-22 23:28:08 +0800

    :pencil: 更新sql文件

--- doc/1schema.sql
+++ doc/1schema.sql
@@ -1,5 +1,2 @@
 -- ballcat
 create database `ballcat` default character set utf8mb4 collate utf8mb4_general_ci;
-
--- ballcat_job xxl-job任务相依赖
-create database `ballcat_job` default character set utf8mb4 collate utf8mb4_general_ci;
