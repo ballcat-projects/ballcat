e028324caf0fdf231f3ae11a28d4ec992c676f05
hccake <chengbohua@foxmail.com>
2020-05-25 21:10:32 +0800

    :zap: admin-core自动装配，简化使用配置

--- ballcat-admin/ballcat-admin-boot/src/main/java/com/your/packages/admin/AdminApplication.java
+++ ballcat-admin/ballcat-admin-boot/src/main/java/com/your/packages/admin/AdminApplication.java
@@ -8,7 +8,6 @@ import com.hccake.simpleredis.EnableSimpleCache;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * @author Hccake
@@ -18,9 +17,8 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @EnableSwagger2Aggregator
 @EnableAccessLog
 @EnableOperationLog
-@ServletComponentScan("com.hccake.ballcat.admin.oauth.filter")
-@SpringBootApplication(scanBasePackages = {"com.hccake.ballcat.admin", "com.your.packages.admin"})
-@MapperScan(basePackages = {"com.hccake.ballcat.**.mapper", "com.your.packages.**.mapper"})
+@MapperScan("com.your.packages.**.mapper")
+@SpringBootApplication
 public class AdminApplication {
 
     public static void main(String[] args) {
