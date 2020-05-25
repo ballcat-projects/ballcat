a2432678cd9c89fe5ce1e90f6b2fe292d3208442
hccake <chengbohua@foxmail.com>
2020-05-25 19:42:22 +0800

    :art: MDC，日志添加TraceId记录

---
+++ ballcat-common/ballcat-common-log/src/main/java/com/hccake/ballcat/commom/log/constant/LogConstant.java
@@ -0,0 +1,16 @@
+package com.hccake.ballcat.commom.log.constant;
+
+/**
+ * @author Hccake
+ * @version 1.0
+ * @date 2020/5/25 17:39
+ */
+public final class LogConstant {
+    private LogConstant(){};
+
+    /**
+     * 跟踪ID，用于一次请求或执行方法时，产生的各种日志间的数据关联
+     */
+    public static final String TRACE_ID = "traceId";
+
+}
