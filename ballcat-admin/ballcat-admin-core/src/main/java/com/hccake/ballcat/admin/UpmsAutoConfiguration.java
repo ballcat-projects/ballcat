de5f3928de319b103cee3fcfeebe59aa09467d21
hccake <chengbohua@foxmail.com>
2020-05-25 21:10:32 +0800

    :zap: admin-core自动装配，简化使用配置

---
+++ ballcat-admin/ballcat-admin-core/src/main/java/com/hccake/ballcat/admin/UpmsAutoConfiguration.java
@@ -0,0 +1,18 @@
+package com.hccake.ballcat.admin;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Hccake
+ * @version 1.0
+ * @date 2020/5/25 21:01
+ */
+@MapperScan("com.hccake.ballcat.**.mapper")
+@ComponentScan
+@ServletComponentScan("com.hccake.ballcat.admin.oauth.filter")
+@Configuration
+public class UpmsAutoConfiguration {
+}
