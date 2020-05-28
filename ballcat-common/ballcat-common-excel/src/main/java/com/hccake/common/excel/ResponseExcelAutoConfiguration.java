a64cfaff07a47808c6960d1d6d4b37b713ac5e37
hccake <chengbohua@foxmail.com>
2020-05-28 13:09:27 +0800

    :art: excle 自动装配

--- ballcat-common/ballcat-common-excel/src/main/java/com/hccake/common/excel/ResponseExcelAutoConfiguration.java
+++ ballcat-common/ballcat-common-excel/src/main/java/com/hccake/common/excel/ResponseExcelAutoConfiguration.java
@@ -3,22 +3,17 @@ package com.hccake.common.excel;
 import com.hccake.common.excel.aop.DynamicNameAspect;
 import com.hccake.common.excel.aop.ResponseExcelReturnValueHandler;
 import com.hccake.common.excel.config.ExcelConfigProperties;
-import com.hccake.common.excel.handler.ManySheetWriteHandler;
 import com.hccake.common.excel.handler.SheetWriteHandler;
-import com.hccake.common.excel.handler.SingleSheetWriteHandler;
+import com.hccake.common.excel.processor.NameProcessor;
+import com.hccake.common.excel.processor.NameSpelExpressionProcessor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
-import com.hccake.common.excel.processor.NameProcessor;
-import com.hccake.common.excel.processor.NameSpelExpressionProcessor;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,10 +27,9 @@ import java.util.List;
 @RequiredArgsConstructor
 @Configuration(proxyBeanMethods = false)
 @EnableConfigurationProperties(ExcelConfigProperties.class)
-public class ResponseExcelAutoConfiguration implements ApplicationContextAware, InitializingBean {
-	private ApplicationContext applicationContext;
+public class ResponseExcelAutoConfiguration implements InitializingBean {
+	private final RequestMappingHandlerAdapter handlerAdapter;
 	private final List<SheetWriteHandler> sheetWriteHandlerList;
-
 
 	/**
 	 * SPEL 解析处理器
@@ -58,32 +52,8 @@ public class ResponseExcelAutoConfiguration implements ApplicationContextAware, 
 		return new DynamicNameAspect(nameProcessor);
 	}
 
-	/**
-	 * 多sheet处理器
-	 * @param configProperties Excle导出配置
-	 * @return ManySheetWriteHandler
-	 */
-	@Bean
-	@ConditionalOnMissingBean
-	public ManySheetWriteHandler manySheetWriteHandler(ExcelConfigProperties configProperties) {
-		return new ManySheetWriteHandler(configProperties);
-	}
-
-	/**
-	 * 单sheet处理器
-	 * @param configProperties Excle导出配置
-	 * @return SingleSheetWriteHandler
-	 */
-	@Bean
-	@ConditionalOnMissingBean
-	public SingleSheetWriteHandler singleSheetWriteHandler(ExcelConfigProperties configProperties) {
-		return new SingleSheetWriteHandler(configProperties);
-	}
-
-
 	@Override
 	public void afterPropertiesSet() {
-		RequestMappingHandlerAdapter handlerAdapter = applicationContext.getBean(RequestMappingHandlerAdapter.class);
 		List<HandlerMethodReturnValueHandler> returnValueHandlers = handlerAdapter.getReturnValueHandlers();
 
 		List<HandlerMethodReturnValueHandler> newHandlers = new ArrayList<>();
@@ -93,8 +63,4 @@ public class ResponseExcelAutoConfiguration implements ApplicationContextAware, 
 		handlerAdapter.setReturnValueHandlers(newHandlers);
 	}
 
-	@Override
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		this.applicationContext = applicationContext;
-	}
 }
--- ballcat-common/ballcat-common-excel/src/main/java/com/hccake/common/excel/aop/DynamicNameAspect.java
+++ ballcat-common/ballcat-common-excel/src/main/java/com/hccake/common/excel/aop/DynamicNameAspect.java
@@ -1,30 +1,37 @@
 package com.hccake.common.excel.aop;
 
-
 import com.hccake.common.excel.annotation.ResponseExcel;
 import com.hccake.common.excel.kit.ExcelNameContextHolder;
+import com.hccake.common.excel.processor.NameProcessor;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.context.annotation.Configuration;
-import com.hccake.common.excel.processor.NameProcessor;
+import org.springframework.stereotype.Component;
 
 /**
  * @author lengleng
  * @date 2020/3/29
  */
 @Aspect
+@Component
 @RequiredArgsConstructor
-@Configuration(proxyBeanMethods = false)
 public class DynamicNameAspect {
 	private final NameProcessor processor;
 
 	@Before("@annotation(excel)")
-	public void around(JoinPoint point, ResponseExcel excel) {
+	public void before(JoinPoint point, ResponseExcel excel) {
 		MethodSignature ms = (MethodSignature) point.getSignature();
 		String name = processor.doDetermineName(point.getArgs(), ms.getMethod(), excel.name());
 		ExcelNameContextHolder.set(name);
 	}
+
+	@AfterThrowing("@annotation(com.hccake.common.excel.annotation.ResponseExcel)")
+	public void afterThrowing(){
+		// 防止异常导致数据未清空
+		ExcelNameContextHolder.clear();
+	}
+
 }
