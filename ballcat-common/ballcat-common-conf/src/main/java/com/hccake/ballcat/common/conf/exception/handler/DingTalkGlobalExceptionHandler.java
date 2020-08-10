841af5fc30fa904862fa420e9136a877249fdbb9
聆听 <sunlisten.gzm@foxmail.com>
2020-08-10 09:43:10 +0800

    :zap: 订单异常通知@所有人

--- ballcat-common/ballcat-common-conf/src/main/java/com/hccake/ballcat/common/conf/exception/handler/DingTalkGlobalExceptionHandler.java
+++ ballcat-common/ballcat-common-conf/src/main/java/com/hccake/ballcat/common/conf/exception/handler/DingTalkGlobalExceptionHandler.java
@@ -25,7 +25,7 @@ public class DingTalkGlobalExceptionHandler extends AbstractNoticeGlobalExceptio
 
 	@Override
 	public ExceptionNoticeResponse send(ExceptionMessage sendMessage) {
-		DingTalkResponse response = sender.sendMessage(new DingTalkTextMessage().setContent(sendMessage.toString()));
+		DingTalkResponse response = sender.sendMessage(new DingTalkTextMessage().setContent(sendMessage.toString()).atAll());
 		return new ExceptionNoticeResponse().setErrMsg(response.getResponse()).setSuccess(response.isSuccess());
 	}
 
