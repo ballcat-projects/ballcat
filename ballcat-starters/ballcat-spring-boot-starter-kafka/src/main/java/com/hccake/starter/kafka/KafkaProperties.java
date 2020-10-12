643c9f132e0b16019f348a1bbddd2f5d6b8425e3
聆听 <lingting@gmail.com>
2020-10-12 17:14:16 +0800

    :sparkles: 添加消费者配置，bean 添加 ConditionalOnMissingBean 注解，用于用户自定义

--- ballcat-starters/ballcat-spring-boot-starter-kafka/src/main/java/com/hccake/starter/kafka/KafkaProperties.java
+++ ballcat-starters/ballcat-spring-boot-starter-kafka/src/main/java/com/hccake/starter/kafka/KafkaProperties.java
@@ -1,13 +1,15 @@
 package com.hccake.starter.kafka;
 
-import lombok.Data;
-import org.apache.kafka.common.serialization.Serializer;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
+import cn.hutool.core.util.StrUtil;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import lombok.Data;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.kafka.common.serialization.Serializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
  * @author lingting 2020/7/28 21:15
@@ -15,6 +17,11 @@ import java.util.Set;
 @Data
 @ConfigurationProperties(prefix = "ballcat.starter.kafka")
 public class KafkaProperties {
+
+	/**
+	 * 用于指定分组
+	 */
+	private String groupId;
 
 	/**
 	 * 所属服务
@@ -27,7 +34,7 @@ public class KafkaProperties {
 	private Class<? extends Serializer<?>> keySerializer = StringSerializer.class;
 
 	/**
-	 * key 序列化 类名
+	 * key 序列化 类名, 如果填写本值则 valueSerializer 无效
 	 */
 	private String keySerializerClassName;
 
@@ -37,13 +44,61 @@ public class KafkaProperties {
 	private Class<? extends Serializer<?>> valueSerializer = StringSerializer.class;
 
 	/**
-	 * value 序列化 类名
+	 * value 序列化 类名, 如果填写本值则 valueSerializer 无效
 	 */
 	private String valueSerializerClassName;
+
+	/**
+	 * key 反序列化
+	 */
+	private Class<? extends Deserializer<?>> keyDeserializer = StringDeserializer.class;
+
+	/**
+	 * key 反序列化 类名, 如果填写本值则 keyDeserializer 无效
+	 */
+	private String keyDeserializerClassName;
+
+	/**
+	 * value 反序列化
+	 */
+	private Class<? extends Deserializer<?>> valueDeserializer = StringDeserializer.class;
+
+	/**
+	 * value 反序列化 类名, 如果填写本值则 valueDeserializer 无效
+	 */
+	private String valueDeserializerClassName;
 
 	/**
 	 * 额外参数
 	 */
 	private Map<String, Object> extend = new HashMap<>();
+
+	public String getKeyDeserializerClassName() {
+		if (StrUtil.isNotEmpty(keyDeserializerClassName)) {
+			return keyDeserializerClassName;
+		}
+		return getKeyDeserializer().getName();
+	}
+
+	public String getValueDeserializerClassName() {
+		if (StrUtil.isNotEmpty(valueDeserializerClassName)) {
+			return valueDeserializerClassName;
+		}
+		return getValueDeserializer().getName();
+	}
+
+	public String getKeySerializerClassName() {
+		if (StrUtil.isNotEmpty(keySerializerClassName)) {
+			return keySerializerClassName;
+		}
+		return getKeySerializer().getName();
+	}
+
+	public String getValueSerializerClassName() {
+		if (StrUtil.isNotEmpty(valueSerializerClassName)) {
+			return valueSerializerClassName;
+		}
+		return getValueSerializer().getName();
+	}
 
 }
