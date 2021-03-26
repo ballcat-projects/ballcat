39acea44bb7eeff5d5956d0b1546c173a64305d8
聆听 <sunlisten.gzm@gmail.com>
2021-03-26 15:41:00 +0800

    :sparkles: lov添加缓存更新(接口检查更新 和 socket通知更新)

---
+++ ballcat-admin/ballcat-admin-core/src/main/java/com/hccake/ballcat/admin/modules/lov/event/LovChangeEvent.java
@@ -0,0 +1,25 @@
+package com.hccake.ballcat.admin.modules.lov.event;
+
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+
+/**
+ * lov 修改事件
+ *
+ * @author lingting 2021/3/25 17:18
+ */
+@Getter
+public class LovChangeEvent extends ApplicationEvent {
+
+	private final String keyword;
+
+	public LovChangeEvent(String keyword) {
+		super(keyword);
+		this.keyword = keyword;
+	}
+
+	public static LovChangeEvent of(String keyword) {
+		return new LovChangeEvent(keyword);
+	}
+
+}
