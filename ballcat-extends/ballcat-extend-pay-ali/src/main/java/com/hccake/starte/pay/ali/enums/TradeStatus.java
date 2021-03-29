9936be7bb100081e3f6179f0e4c03bfeb43a4546
聆听 <sunlisten.gzm@gmail.com>
2021-03-29 15:24:19 +0800

    :art: 支付宝交易状态枚举添加原始信息字段

--- ballcat-extends/ballcat-extend-pay-ali/src/main/java/com/hccake/starte/pay/ali/enums/TradeStatus.java
+++ ballcat-extends/ballcat-extend-pay-ali/src/main/java/com/hccake/starte/pay/ali/enums/TradeStatus.java
@@ -1,49 +1,56 @@
 package com.hccake.starte.pay.ali.enums;
 
+import cn.hutool.core.util.StrUtil;
 import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.var;
 
 /**
  * 交易状态
  */
+@Getter
+@AllArgsConstructor
 public enum TradeStatus {
 
 	/**
 	 * 成功
 	 */
-	SUCCESS,
+	SUCCESS("TRADE_SUCCESS"),
 	/**
 	 * 未支付
 	 */
-	WAIT,
+	WAIT("WAIT_BUYER_PAY"),
 	/**
 	 * 未付款交易超时关闭，或支付完成后全额退款
 	 */
-	CLOSED,
+	CLOSED("TRADE_CLOSED"),
 	/**
 	 * 交易结束，不可退款
 	 */
-	FINISHED,
+	FINISHED("TRADE_FINISHED"),
 	/**
 	 * 异常. 具体信息查询 subCode和subMsg
 	 */
-	ERROR,
+	ERROR(""),
 
 	;
 
+	private final String str;
+
 	@JsonCreator
 	public static TradeStatus of(String status) {
-		switch (status) {
-		case "WAIT_BUYER_PAY":
-			return WAIT;
-		case "TRADE_CLOSED":
-			return CLOSED;
-		case "TRADE_SUCCESS":
-			return SUCCESS;
-		case "TRADE_FINISHED":
-			return FINISHED;
-		default:
+		if (StrUtil.isBlank(status)) {
 			return ERROR;
 		}
+
+		for (var e : values()) {
+			if (e.getStr().equals(status)) {
+				return e;
+			}
+		}
+
+		return ERROR;
 	}
 
 }
