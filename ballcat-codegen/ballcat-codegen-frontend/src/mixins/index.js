458325f55778d1856f92b55dc4d680f0d9d0ca8a
hccake <chengbohua@foxmail.com>
2021-03-30 21:18:23 +0800

    :fire: 代码生成器迁出作为独立项目存在 https://github.com/ballcat-projects/ballcat-codegen

--- ballcat-codegen/ballcat-codegen-frontend/src/mixins/index.js
+++
@@ -1,6 +0,0 @@
-import TablePageMixin from './tablePageMixin'
-import FormMixin from './formMixin'
-import FormPageMixin from './formPageMixin'
-import PopUpFormMixin from './popUpFormMixin'
-
-export { TablePageMixin, FormMixin, FormPageMixin, PopUpFormMixin }
