130c6265dbfcb4d32bc3015631646ae40a13f744
聆听 <sunlisten.gzm@gmail.com>
2021-04-14 20:45:10 +0800

    :sparkles: BusinessException 添加 用于需要format返回结果的构造函数

--- ballcat-common/ballcat-common-core/src/main/java/com/hccake/ballcat/common/core/exception/BusinessException.java
+++ ballcat-common/ballcat-common-core/src/main/java/com/hccake/ballcat/common/core/exception/BusinessException.java
@@ -1,5 +1,6 @@
 package com.hccake.ballcat.common.core.exception;
 
+import cn.hutool.core.util.StrUtil;
 import com.hccake.ballcat.common.model.result.ResultCode;
 import lombok.Getter;
 
@@ -21,10 +22,26 @@ public class BusinessException extends RuntimeException {
 		this.message = resultCode.getMessage();
 	}
 
+	/**
+	 * 用于需要format返回结果的异常
+	 * @author lingting 2021-04-13 14:25
+	 */
+	public BusinessException(ResultCode resultCode, Object... args) {
+		this(resultCode.getCode(), StrUtil.format(resultCode.getMessage(), args));
+	}
+
 	public BusinessException(ResultCode resultCode, Throwable e) {
 		super(resultCode.getMessage(), e);
 		this.code = resultCode.getCode();
 		this.message = resultCode.getMessage();
+	}
+
+	/**
+	 * 用于需要format返回结果的异常
+	 * @author lingting 2021-04-13 14:25
+	 */
+	public BusinessException(ResultCode resultCode, Throwable e, Object... args) {
+		this(resultCode.getCode(), StrUtil.format(resultCode.getMessage(), args), e);
 	}
 
 	public BusinessException(int code, String message) {
