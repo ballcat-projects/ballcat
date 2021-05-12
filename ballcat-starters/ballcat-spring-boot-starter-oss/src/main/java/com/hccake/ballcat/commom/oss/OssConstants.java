467cfc5c2a90fe182510345248ff5f5495c386c1
聆听 <sunlisten.gzm@gmail.com>
2021-05-12 17:09:19 +0800

    :recycle: 重构 OssClient 生成代码. 兼容其他平台的s3接口, 弃用路径形式改用host形式来指定 bucket

--- ballcat-starters/ballcat-spring-boot-starter-oss/src/main/java/com/hccake/ballcat/commom/oss/OssConstants.java
+++ ballcat-starters/ballcat-spring-boot-starter-oss/src/main/java/com/hccake/ballcat/commom/oss/OssConstants.java
@@ -11,4 +11,18 @@ public class OssConstants {
 
 	public static final String PATH_FLAG = "/";
 
+	/**
+	 * 亚马逊 国际 后缀
+	 */
+	public static final String AWS_INTERNATIONAL = "amazonaws.com";
+
+	/**
+	 * 亚马逊 国内 后缀
+	 */
+	public static final String AWS_CN = "amazonaws.com.cn";
+
+	public static final String DOT = ".";
+
+	public static final String S3 = "s3.";
+
 }
