6986d660e79f6877ff93098063277f7a59e5ddd6
Hccake <chengbohua@foxmail.com>
2021-05-29 18:53:56 +0800

    :sparkles: 扩展 TreeUtils，提供排序功能

--- ballcat-common/ballcat-common-util/src/main/java/com/hccake/ballcat/common/util/TreeUtils.java
+++ ballcat-common/ballcat-common-util/src/main/java/com/hccake/ballcat/common/util/TreeUtils.java
@@ -5,11 +5,10 @@ import com.hccake.ballcat.common.model.tree.TreeNode;
 import lombok.experimental.UtilityClass;
 import org.springframework.util.CollectionUtils;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author Hccake 2020/6/21 17:21
@@ -27,7 +26,20 @@ public class TreeUtils {
 	 * @return 树列表
 	 */
 	public <T extends TreeNode<I>, I> List<T> buildTree(List<T> nodes, I rootId) {
-		return TreeUtils.buildTree(nodes, rootId, Function.identity());
+		return TreeUtils.buildTree(nodes, rootId, Function.identity(), null);
+	}
+
+	/**
+	 * 根据一个TreeNode集合，返回构建好的树列表
+	 * @param nodes TreeNode集合
+	 * @param rootId 根节点Id
+	 * @param comparator 树节点排序规则
+	 * @param <T> TreeNode的子类
+	 * @param <I> TreeNodeId的类型
+	 * @return 树列表
+	 */
+	public <T extends TreeNode<I>, I> List<T> buildTree(List<T> nodes, I rootId, Comparator<? super T> comparator) {
+		return TreeUtils.buildTree(nodes, rootId, Function.identity(), comparator);
 	}
 
 	/**
@@ -41,11 +53,35 @@ public class TreeUtils {
 	 * @return 树列表
 	 */
 	public <T extends TreeNode<I>, I, R> List<T> buildTree(List<R> list, I rootId, Function<R, T> convertToTree) {
+		return TreeUtils.buildTree(list, rootId, convertToTree, null);
+	}
+
+	/**
+	 * 根据源数据列表转换为树
+	 * @param list 源数据列表
+	 * @param rootId 根节点Id
+	 * @param convertToTree 转换方法
+	 * @param comparator 树节点排序规则
+	 * @param <T> TreeNode的子类
+	 * @param <I> TreeNodeId的类型
+	 * @param <R> 源数据类型
+	 * @return 树列表
+	 */
+	public <T extends TreeNode<I>, I, R> List<T> buildTree(List<R> list, I rootId, Function<R, T> convertToTree,
+			Comparator<? super T> comparator) {
 		if (list == null || list.isEmpty()) {
 			return new ArrayList<>();
 		}
+
+		// 转换为 TreeNode
+		Stream<T> tStream = list.stream().map(convertToTree);
+		// 如果需要排序，则在收集时进行排序处理
+		if (comparator != null) {
+			tStream = tStream.sorted(comparator);
+		}
 		// 根据 parentId 进行分组
-		Map<I, List<T>> childrenMap = list.stream().map(convertToTree).collect(Collectors.groupingBy(T::getParentId));
+		Map<I, List<T>> childrenMap = tStream
+				.collect(Collectors.groupingBy(T::getParentId, LinkedHashMap::new, Collectors.toList()));
 
 		// 根据根节点ID拿到一级节点
 		List<T> treeList = childrenMap.get(rootId);
