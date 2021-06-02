d88d404252714ff9444e49e227944716365e1295
Hccake <chengbohua@foxmail.com>
2021-06-02 11:33:19 +0800

    :bug: 修复在重复引入common-conf时，由于配置加载顺序导致的 FillMetaObjectHandle 初始化异常

--- ballcat-admin/ballcat-admin-core/src/main/java/com/hccake/ballcat/admin/config/AdminMybatisPlusConfig.java
+++ ballcat-admin/ballcat-admin-core/src/main/java/com/hccake/ballcat/admin/config/AdminMybatisPlusConfig.java
@@ -1,6 +1,9 @@
 package com.hccake.ballcat.admin.config;
 
 import com.hccake.ballcat.common.conf.mybatis.FillMetaObjectHandle;
+import com.hccake.ballcat.common.conf.mybatis.MybatisPlusConfig;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
  * @author hccake
  * @date 2020/12/16
  */
+@AutoConfigureBefore(MybatisPlusConfig.class)
 @Configuration(proxyBeanMethods = false)
 public class AdminMybatisPlusConfig {
 
@@ -16,6 +20,7 @@ public class AdminMybatisPlusConfig {
 	 * @return FillMetaObjectHandle
 	 */
 	@Bean
+	@ConditionalOnMissingBean
 	public FillMetaObjectHandle fillMetaObjectHandle() {
 		return new AdminFillMetaObjectHandle();
 	}
