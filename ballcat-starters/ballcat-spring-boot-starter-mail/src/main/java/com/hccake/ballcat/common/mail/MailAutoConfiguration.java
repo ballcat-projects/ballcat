f683773f2a95550d5ae70002aa0ac480ac26b51b
聆听 <sunlisten.gzm@gmail.com>
2021-06-11 14:57:32 +0800

    :bug: 修复在未配置邮箱时, 注册 MailSender bean导致启动异常

--- ballcat-starters/ballcat-spring-boot-starter-mail/src/main/java/com/hccake/ballcat/common/mail/MailAutoConfiguration.java
+++ ballcat-starters/ballcat-spring-boot-starter-mail/src/main/java/com/hccake/ballcat/common/mail/MailAutoConfiguration.java
@@ -4,6 +4,7 @@ import com.hccake.ballcat.common.mail.sender.MailSender;
 import com.hccake.ballcat.common.mail.sender.MailSenderImpl;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
@@ -20,6 +21,7 @@ public class MailAutoConfiguration {
 
 	@Bean
 	@ConditionalOnMissingBean(MailSender.class)
+	@ConditionalOnProperty(prefix = "spring.mail", name = "host")
 	public MailSender mailSenderImpl(JavaMailSender javaMailSender,
 			ApplicationEventPublisher applicationEventPublisher) {
 		return new MailSenderImpl(javaMailSender, applicationEventPublisher);
