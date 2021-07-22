c3be76658bc3d31462147d2c1fc5e52720bbb3ca
聆听 <sunlisten.gzm@gmail.com>
2021-07-22 16:56:29 +0800

    :sparkles: 添加StreamUtils 用于处理流

---
+++ ballcat-common/ballcat-common-util/src/main/java/com/hccake/ballcat/common/util/StreamUtils.java
@@ -0,0 +1,80 @@
+package com.hccake.ballcat.common.util;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author lingting 2021/4/21 17:45
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class StreamUtils {
+
+	/**
+	 * 克隆文件流
+	 * @param stream 源流
+	 * @param amounts 数量
+	 * @author lingting 2021-04-16 16:18
+	 */
+	public static InputStream[] clone(InputStream stream, Integer amounts) throws IOException {
+		InputStream[] streams = new InputStream[amounts];
+		ByteArrayOutputStream[] outs = new ByteArrayOutputStream[amounts];
+
+		byte[] buffer = new byte[1024];
+		int len;
+
+		while ((len = stream.read(buffer)) > -1) {
+
+			for (int i = 0, outsLength = outs.length; i < outsLength; i++) {
+				ByteArrayOutputStream out = outs[i];
+				if (out == null) {
+					out = new ByteArrayOutputStream();
+					outs[i] = out;
+				}
+				out.write(buffer, 0, len);
+			}
+		}
+
+		for (int i = 0; i < outs.length; i++) {
+			streams[i] = new ByteArrayInputStream(outs[i].toByteArray());
+		}
+
+		return streams;
+	}
+
+	/**
+	 * 流转字符串
+	 * @author lingting 2021-04-29 18:07
+	 */
+	public static String toString(InputStream stream) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8));
+		StringBuilder stringBuilder = new StringBuilder();
+		String line;
+		while ((line = reader.readLine()) != null) {
+			stringBuilder.append(line);
+		}
+		return stringBuilder.toString();
+	}
+
+	/**
+	 * 从流中读取 int
+	 * @author lingting 2021-07-22 14:54
+	 */
+	public static int readInt(InputStream is, int noOfBytes, boolean bigEndian) throws IOException {
+		int ret = 0;
+		int sv = bigEndian ? ((noOfBytes - 1) * 8) : 0;
+		int cnt = bigEndian ? -8 : 8;
+		for (int i = 0; i < noOfBytes; i++) {
+			ret |= is.read() << sv;
+			sv += cnt;
+		}
+		return ret;
+	}
+
+}
