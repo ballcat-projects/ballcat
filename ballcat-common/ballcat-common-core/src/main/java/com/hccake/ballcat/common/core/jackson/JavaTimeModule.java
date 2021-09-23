7f61db74437c9acb5ef443717426e727bd804211
Hccake <chengbohua@foxmail.com>
2021-09-23 19:03:40 +0800

    :bug: fix InvalidDefinitionException: Java 8 date/time type `java.time.Instant` not supported by default

--- ballcat-common/ballcat-common-core/src/main/java/com/hccake/ballcat/common/core/jackson/JavaTimeModule.java
+++ ballcat-common/ballcat-common-core/src/main/java/com/hccake/ballcat/common/core/jackson/JavaTimeModule.java
@@ -3,13 +3,16 @@ package com.hccake.ballcat.common.core.jackson;
 import cn.hutool.core.date.DatePattern;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.PackageVersion;
+import com.fasterxml.jackson.datatype.jsr310.deser.InstantDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.InstantSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -18,24 +21,25 @@ import java.time.format.DateTimeFormatter;
 /**
  * 自定义java8新增时间类型的序列化
  *
+ * @see com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
  * @author Hccake
  */
 public class JavaTimeModule extends SimpleModule {
 
 	public JavaTimeModule() {
 		super(PackageVersion.VERSION);
+
 		this.addSerializer(LocalDateTime.class,
 				new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DatePattern.NORM_DATETIME_PATTERN)));
-		this.addSerializer(LocalDate.class,
-				new LocalDateSerializer(DateTimeFormatter.ofPattern(DatePattern.NORM_DATE_PATTERN)));
-		this.addSerializer(LocalTime.class,
-				new LocalTimeSerializer(DateTimeFormatter.ofPattern(DatePattern.NORM_TIME_PATTERN)));
+		this.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ISO_LOCAL_DATE));
+		this.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ISO_LOCAL_TIME));
+		this.addSerializer(Instant.class, InstantSerializer.INSTANCE);
+
 		this.addDeserializer(LocalDateTime.class,
 				new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DatePattern.NORM_DATETIME_PATTERN)));
-		this.addDeserializer(LocalDate.class,
-				new LocalDateDeserializer(DateTimeFormatter.ofPattern(DatePattern.NORM_DATE_PATTERN)));
-		this.addDeserializer(LocalTime.class,
-				new LocalTimeDeserializer(DateTimeFormatter.ofPattern(DatePattern.NORM_TIME_PATTERN)));
+		this.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ISO_LOCAL_DATE));
+		this.addDeserializer(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ISO_LOCAL_TIME));
+		this.addDeserializer(Instant.class, InstantDeserializer.INSTANT);
 	}
 
 }
