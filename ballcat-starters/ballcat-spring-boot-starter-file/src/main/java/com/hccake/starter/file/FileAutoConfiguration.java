fa22e7351696e864c0a3182f822e9f91086439e8
lingting <sunlisten.gzm@gmail.com>
2021-10-19 23:29:26 +0800

    :zap: 调整start-file的依赖和bean注册逻辑

--- ballcat-starters/ballcat-spring-boot-starter-file/src/main/java/com/hccake/starter/file/FileAutoConfiguration.java
+++ ballcat-starters/ballcat-spring-boot-starter-file/src/main/java/com/hccake/starter/file/FileAutoConfiguration.java
@@ -1,9 +1,6 @@
 package com.hccake.starter.file;
 
-import com.hccake.ballcat.common.oss.OssClient;
-import com.hccake.starter.ftp.FtpClient;
 import lombok.AllArgsConstructor;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
@@ -15,8 +12,8 @@ import org.springframework.context.annotation.Bean;
 public class FileAutoConfiguration {
 
 	@Bean
-	@ConditionalOnMissingBean({OssClient.class, FtpClient.class})
 	public FileClient ballcatFileLocalClient(FileProperties properties) throws FileException {
 		return new FileClient(properties);
 	}
+
 }
