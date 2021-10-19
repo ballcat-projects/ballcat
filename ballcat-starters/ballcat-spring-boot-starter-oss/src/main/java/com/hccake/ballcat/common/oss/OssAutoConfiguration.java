af4172e614ee7007d03fd084322fdb2aa7767121
lingting <sunlisten.gzm@gmail.com>
2021-10-19 22:35:46 +0800

    :zap: 优化OssClient注册逻辑

--- ballcat-starters/ballcat-spring-boot-starter-oss/src/main/java/com/hccake/ballcat/common/oss/OssAutoConfiguration.java
+++ ballcat-starters/ballcat-spring-boot-starter-oss/src/main/java/com/hccake/ballcat/common/oss/OssAutoConfiguration.java
@@ -3,6 +3,7 @@ package com.hccake.ballcat.common.oss;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -19,6 +20,7 @@ public class OssAutoConfiguration {
 	@Bean
 	@ConditionalOnClass(S3Client.class)
 	@ConditionalOnMissingBean(OssClient.class)
+	@ConditionalOnProperty(prefix = "ballcat.oss", name = "accessKey")
 	public OssClient ossClient(OssProperties properties) {
 		return new OssClient(properties);
 	}
