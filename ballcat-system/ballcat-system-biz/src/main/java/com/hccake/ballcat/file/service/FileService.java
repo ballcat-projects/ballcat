08d07ed8a72e65bd0fd1dfe152c08d496b88c4f3
lingting <sunlisten.gzm@gmail.com>
2021-10-19 23:29:42 +0800

    :zap: 完善FileService

--- ballcat-system/ballcat-system-biz/src/main/java/com/hccake/ballcat/file/service/FileService.java
+++ ballcat-system/ballcat-system-biz/src/main/java/com/hccake/ballcat/file/service/FileService.java
@@ -1,51 +1,48 @@
 package com.hccake.ballcat.file.service;
 
 import com.hccake.ballcat.common.oss.OssClient;
-import com.hccake.ballcat.common.core.exception.BusinessException;
-import com.hccake.ballcat.common.model.result.BaseResultCode;
-import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
-import org.springframework.stereotype.Component;
-
-import java.io.File;
-import java.io.FileOutputStream;
+import com.hccake.ballcat.common.util.SpringUtils;
+import com.hccake.starter.file.FileClient;
 import java.io.InputStream;
+import lombok.SneakyThrows;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.stereotype.Component;
 
 /**
  * @author lingting 2021/5/27 11:14
  */
 @Component
-@RequiredArgsConstructor
+@DependsOn("springUtils")
 public class FileService {
 
-	private final OssClient ossClient;
+	private OssClient ossClient;
+
+	private final FileClient fileClient;
+
+	public FileService() {
+		try {
+			ossClient = SpringUtils.getBean(OssClient.class);
+		}
+		catch (Exception ignore) {
+			ossClient = null;
+		}
+
+		// oss 为空或者未配置
+		if (ossClient == null || !ossClient.isEnable()) {
+			fileClient = SpringUtils.getBean(FileClient.class);
+		}
+		else {
+			fileClient = null;
+		}
+	}
 
 	@SneakyThrows
 	public String upload(InputStream stream, String relativePath, Long size) {
-		String path;
+		if (fileClient != null) {
+			return fileClient.upload(stream, relativePath);
+		}
 
-		if (ossClient.enabled()) {
-			path = ossClient.upload(stream, relativePath, size);
-		}
-		else {
-			final File file = new File(relativePath);
-			if (!file.exists()) {
-				// 创建所需的父级目录. 可能已存在
-				file.getParentFile().mkdirs();
-				// 创建空文件
-				if (!file.createNewFile()) {
-					throw new BusinessException(BaseResultCode.FILE_UPLOAD_ERROR.getCode(), "文件创建失败!");
-				}
-				try (FileOutputStream fileStream = new FileOutputStream(file)) {
-					byte[] line = new byte[2048];
-					while (stream.read(line) > 0) {
-						fileStream.write(line);
-					}
-				}
-			}
-			path = relativePath;
-		}
-		return path;
+		return ossClient.upload(stream, relativePath, size);
 	}
 
 }
