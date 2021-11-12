3e6d400f53abeec6b7beecacd1cff1f7f03eea06
Hccake <chengbohua@foxmail.com>
2021-11-12 22:17:48 +0800

    :bug: 修复无法在聚合者中对 provider 进行调试的问题

--- ballcat-starters/ballcat-spring-boot-starter-swagger/src/main/java/com/hccake/ballcat/common/swagger/SwaggerProviderAutoConfiguration.java
+++ ballcat-starters/ballcat-spring-boot-starter-swagger/src/main/java/com/hccake/ballcat/common/swagger/SwaggerProviderAutoConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -50,10 +51,9 @@ public class SwaggerProviderAutoConfiguration {
 		}
 		config.addAllowedHeader(ALL);
 		config.addAllowedMethod(ALL);
-		source.registerCorsConfiguration("/v2/api-docs**", config);
-		source.registerCorsConfiguration("/v3/api-docs**", config);
+		source.registerCorsConfiguration("/**", config);
 		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
-		bean.setOrder(0);
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
 		return bean;
 	}
 
