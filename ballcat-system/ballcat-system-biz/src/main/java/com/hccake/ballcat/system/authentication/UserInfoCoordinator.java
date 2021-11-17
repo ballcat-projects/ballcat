cbbe2729ff081b98cce520f9a7bb6f48cc0df7cd
Author.zero <1090863704@qq.com>
2021-11-17 22:47:33 +0800

    :art: 修改UserInfoCoordinator为接口提供默认实现,修改LogicDeletedBaseEntity的注释问题

--- ballcat-system/ballcat-system-biz/src/main/java/com/hccake/ballcat/system/authentication/UserInfoCoordinator.java
+++ ballcat-system/ballcat-system-biz/src/main/java/com/hccake/ballcat/system/authentication/UserInfoCoordinator.java
@@ -5,21 +5,19 @@ import com.hccake.ballcat.system.model.dto.UserInfoDTO;
 import java.util.Map;
 
 /**
- * 用户信息协调者 子类重写该类，用于对用户资源或者用户属性的自定义调整
+ * 用户附属属性协调
  *
- * @author Hccake 2020/9/28
- * @version 1.0
+ * @author author.zero
+ * @version 1.0 2021/11/16 20:02
  */
-public class UserInfoCoordinator {
+public interface UserInfoCoordinator {
 
 	/**
-	 * 用户附属属性协调 对于不同类型的用户，可能在业务上需要获取到不同的用户属性 子类重写此方法，进行用户属性的增强
+	 * 对于不同类型的用户，可能在业务上需要获取到不同的用户属性 实现此接口，进行用户属性的增强
 	 * @param userInfoDTO 系统用户信息
 	 * @param attribute 用户属性，默认添加了 roles 和 permissions 属性
 	 * @return attribute
 	 */
-	public Map<String, Object> coordinateAttribute(UserInfoDTO userInfoDTO, Map<String, Object> attribute) {
-		return attribute;
-	}
+	Map<String, Object> coordinateAttribute(UserInfoDTO userInfoDTO, Map<String, Object> attribute);
 
 }
