8ea7fea1cc8be43f2d43658f976d0ee80dfa3bfa
lingting <sunlisten.gzm@gmail.com>
2022-01-13 18:32:54 +0800

    :art: OssDisabledException.java 改为 RuntimeException

--- ballcat-starters/ballcat-spring-boot-starter-oss/src/main/java/com/hccake/ballcat/common/oss/exception/OssDisabledException.java
+++ ballcat-starters/ballcat-spring-boot-starter-oss/src/main/java/com/hccake/ballcat/common/oss/exception/OssDisabledException.java
@@ -3,7 +3,7 @@ package com.hccake.ballcat.common.oss.exception;
 /**
  * @author lingting 2021/5/27 10:44
  */
-public class OssDisabledException extends Exception {
+public class OssDisabledException extends RuntimeException {
 
 	public OssDisabledException() {
 		super("oss 未启用!");
