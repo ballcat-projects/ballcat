435cc8873bab548e3e10b5ac922ef8f5848a154c
Hccake <chengbohua@foxmail.com>
2022-06-25 11:19:13 +0800

    :bug: (xss) 修复反序列化时的异常问题

--- ballcat-starters/ballcat-spring-boot-starter-xss/src/main/java/com/hccake/ballcat/common/xss/core/XssStringJsonDeserializer.java
+++ ballcat-starters/ballcat-spring-boot-starter-xss/src/main/java/com/hccake/ballcat/common/xss/core/XssStringJsonDeserializer.java
@@ -1,8 +1,9 @@
 package com.hccake.ballcat.common.xss.core;
 
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
 import com.hccake.ballcat.common.xss.cleaner.XssCleaner;
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,7 +17,7 @@ import java.io.IOException;
  * @date 2019/10/17 22:23
  */
 @Slf4j
-public class XssStringJsonDeserializer extends JsonDeserializer<String> {
+public class XssStringJsonDeserializer extends StringDeserializer {
 
 	private final XssCleaner xssCleaner;
 
@@ -26,17 +27,45 @@ public class XssStringJsonDeserializer extends JsonDeserializer<String> {
 
 	@Override
 	public String deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
-		String value = p.getValueAsString();
-		// 没开启 Xss 则直接返回
-		if (!XssStateHolder.enabled()) {
-			return value;
+		if (p.hasToken(JsonToken.VALUE_STRING)) {
+			return getCleanText(p.getText());
 		}
-		return value != null ? xssCleaner.clean(value) : null;
+		JsonToken t = p.currentToken();
+		// [databind#381]
+		if (t == JsonToken.START_ARRAY) {
+			return _deserializeFromArray(p, ctxt);
+		}
+		// need to gracefully handle byte[] data, as base64
+		if (t == JsonToken.VALUE_EMBEDDED_OBJECT) {
+			Object ob = p.getEmbeddedObject();
+			if (ob == null) {
+				return null;
+			}
+			if (ob instanceof byte[]) {
+				return ctxt.getBase64Variant().encode((byte[]) ob, false);
+			}
+			// otherwise, try conversion using toString()...
+			return ob.toString();
+		}
+		// 29-Jun-2020, tatu: New! "Scalar from Object" (mostly for XML)
+		if (t == JsonToken.START_OBJECT) {
+			return ctxt.extractScalarFromObject(p, this, _valueClass);
+		}
+		// allow coercions for other scalar types
+		// 17-Jan-2018, tatu: Related to [databind#1853] avoid FIELD_NAME by ensuring it's
+		// "real" scalar
+		if (t.isScalarValue()) {
+			String text = p.getValueAsString();
+			return getCleanText(text);
+		}
+		return (String) ctxt.handleUnexpectedToken(_valueClass, p);
 	}
 
-	@Override
-	public Class<String> handledType() {
-		return String.class;
+	private String getCleanText(String text) {
+		if (text == null) {
+			return null;
+		}
+		return XssStateHolder.enabled() ? xssCleaner.clean(text) : text;
 	}
 
 }
