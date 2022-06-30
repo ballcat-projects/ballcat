01c7fbe3076efc221aea0bab3842b72a4f710a2e
liu_yx <elliotxiang@163.com>
2022-06-30 14:19:44 +0800

    ✨扩展Rocketmq消息发送

---
+++ ballcat-starters/ballcat-spring-boot-starter-websocket/src/main/java/com/hccake/ballcat/autoconfigure/websocket/config/RocketMqMessageDistributorConfig.java
@@ -0,0 +1,36 @@
+package com.hccake.ballcat.autoconfigure.websocket.config;
+
+import com.hccake.ballcat.autoconfigure.websocket.MessageDistributorTypeConstants;
+import com.hccake.ballcat.autoconfigure.websocket.WebSocketProperties;
+import com.hccake.ballcat.common.websocket.distribute.MessageDistributor;
+import com.hccake.ballcat.common.websocket.distribute.RocketmqMessageDistributor;
+import com.hccake.ballcat.common.websocket.session.WebSocketSessionStore;
+import lombok.RequiredArgsConstructor;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ClassName RocketMqMessageDistributorConfig.java
+ * @Author liu_yx
+ * @Version 1.0.0
+ * @Description MQ的消息分发器配置
+ * @CreateTime 2022年06月30日 14:11:34
+ */
+@ConditionalOnProperty(prefix = WebSocketProperties.PREFIX, name = "message-distributor",
+		havingValue = MessageDistributorTypeConstants.ROCKETMQ, matchIfMissing = true)
+@Configuration(proxyBeanMethods = false)
+@RequiredArgsConstructor
+public class RocketMqMessageDistributorConfig {
+
+	private final WebSocketSessionStore webSocketSessionStore;
+
+	@Bean
+	@ConditionalOnMissingBean(MessageDistributor.class)
+	public RocketmqMessageDistributor messageDistributor(RocketMQTemplate template) {
+		return new RocketmqMessageDistributor(webSocketSessionStore, template);
+	}
+
+}
