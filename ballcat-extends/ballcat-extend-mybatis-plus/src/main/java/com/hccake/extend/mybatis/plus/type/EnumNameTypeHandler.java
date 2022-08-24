4e0b3d0aa764de1242efed7d209d86571df69c41
lingting <sunlisten.gzm@gmail.com>
2022-08-24 17:06:26 +0800

    :bug: 修复 EnumNameTypeHandler 为默认枚举类型处理器时 处理实现IEnum的枚举值获取异常

--- ballcat-extends/ballcat-extend-mybatis-plus/src/main/java/com/hccake/extend/mybatis/plus/type/EnumNameTypeHandler.java
+++ ballcat-extends/ballcat-extend-mybatis-plus/src/main/java/com/hccake/extend/mybatis/plus/type/EnumNameTypeHandler.java
@@ -1,12 +1,13 @@
 package com.hccake.extend.mybatis.plus.type;
 
+import com.baomidou.mybatisplus.annotation.IEnum;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
 import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import org.apache.ibatis.type.BaseTypeHandler;
-import org.apache.ibatis.type.JdbcType;
-import org.springframework.util.StringUtils;
 
 /**
  * 普通枚举类型处理. 根据 name() 返回值判断枚举值
@@ -29,39 +30,59 @@ public class EnumNameTypeHandler<E extends Enum<E>> extends BaseTypeHandler<E> {
 		if (parameter == null) {
 			ps.setString(i, "");
 		}
-		else if (jdbcType == null) {
-			ps.setString(i, parameter.name());
-		}
 		else {
-			ps.setObject(i, parameter.name(), jdbcType.TYPE_CODE);
+			Object val = getValByEnum(parameter);
+			if (jdbcType == null) {
+				ps.setString(i, val == null ? null : val.toString());
+			}
+			else {
+				ps.setObject(i, val, jdbcType.TYPE_CODE);
+			}
 		}
 	}
 
 	@Override
 	public E getNullableResult(ResultSet rs, String columnName) throws SQLException {
-		String s = rs.getString(columnName);
-		return StringUtils.hasText(s) ? getEnumByName(s) : null;
+		return getEnumByName(rs.getString(columnName));
 	}
 
 	@Override
 	public E getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
-		String s = rs.getString(columnIndex);
-		return StringUtils.hasText(s) ? getEnumByName(s) : null;
+		return getEnumByName(rs.getString(columnIndex));
 	}
 
 	@Override
 	public E getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
-		String s = cs.getString(columnIndex);
-		return StringUtils.hasText(s) ? getEnumByName(s) : null;
+		return getEnumByName(cs.getString(columnIndex));
+	}
+
+	boolean isIEnum(E e) {
+		return IEnum.class.isAssignableFrom(e.getClass());
+	}
+
+	Object getValByEnum(E e) {
+		// IEnum
+		if (isIEnum(e)) {
+			return ((IEnum<?>) e).getValue();
+		}
+		return e.name();
 	}
 
 	/**
 	 * 根据枚举 name() 获取枚举
 	 * @author lingting 2021-06-07 13:50
 	 */
-	E getEnumByName(String name) {
+	E getEnumByName(String val) {
 		for (E e : type.getEnumConstants()) {
-			if (e.name().equals(name)) {
+			Object ev = getValByEnum(e);
+			if (ev == null) {
+				if (val == null) {
+					return e;
+				}
+				continue;
+			}
+
+			if (val.equals(ev.toString())) {
 				return e;
 			}
 		}
