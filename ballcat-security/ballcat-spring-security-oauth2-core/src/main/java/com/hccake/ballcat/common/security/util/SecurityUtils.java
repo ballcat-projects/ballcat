243af735e1611ed03599c5f2c541891eca62e2ec
Hccake <chengbohua@foxmail.com>
2022-09-30 19:18:24 +0800

    :fire: 移除弃用的测试客户端判断方法

--- ballcat-security/ballcat-spring-security-oauth2-core/src/main/java/com/hccake/ballcat/common/security/util/SecurityUtils.java
+++ ballcat-security/ballcat-spring-security-oauth2-core/src/main/java/com/hccake/ballcat/common/security/util/SecurityUtils.java
@@ -1,14 +1,10 @@
 package com.hccake.ballcat.common.security.util;
 
-import com.hccake.ballcat.common.security.constant.SecurityConstants;
 import com.hccake.ballcat.common.security.userdetails.ClientPrincipal;
 import com.hccake.ballcat.common.security.userdetails.User;
 import lombok.experimental.UtilityClass;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Optional;
 
 /**
  * @author Hccake
@@ -48,18 +44,6 @@ public class SecurityUtils {
 	public User getUser() {
 		Authentication authentication = getAuthentication();
 		return getUser(authentication);
-	}
-
-	/**
-	 * 判断当前是否是测试客户端
-	 * @return boolean 是：true，否：false
-	 */
-	public boolean isTestClient() {
-		// 测试客户端 跳过密码解密（swagger 或 postman测试时使用）
-		Authentication authentication = SecurityUtils.getAuthentication();
-		UserDetails userDetails = (UserDetails) Optional.ofNullable(authentication).map(Authentication::getPrincipal)
-				.orElse(null);
-		return userDetails != null && SecurityConstants.TEST_CLIENT_ID.equals(userDetails.getUsername());
 	}
 
 	/**
