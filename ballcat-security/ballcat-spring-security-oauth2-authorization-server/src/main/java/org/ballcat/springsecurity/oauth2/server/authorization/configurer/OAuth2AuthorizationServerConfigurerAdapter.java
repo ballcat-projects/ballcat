ca35245b057173bc5f55cb1ae992ab89eb7827d3
Hccake <chengbohua@foxmail.com>
2022-10-08 20:26:28 +0800

    :sparkles: 添加 OAuth2AuthorizationServerConfigurerCustomizer, 方便用户扩展配置(#gh-202)

--- ballcat-security/ballcat-spring-security-oauth2-authorization-server/src/main/java/org/ballcat/springsecurity/oauth2/server/authorization/configurer/OAuth2AuthorizationServerConfigurerAdapter.java
+++ ballcat-security/ballcat-spring-security-oauth2-authorization-server/src/main/java/org/ballcat/springsecurity/oauth2/server/authorization/configurer/OAuth2AuthorizationServerConfigurerAdapter.java
@@ -1,10 +1,15 @@
 package org.ballcat.springsecurity.oauth2.server.authorization.configurer;
 
 import lombok.RequiredArgsConstructor;
+import org.ballcat.security.captcha.CaptchaValidator;
+import org.ballcat.security.properties.SecurityProperties;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import java.util.List;
 
 /**
  * OAuth2 的授权服务配置
@@ -17,17 +22,30 @@ import org.springframework.security.web.util.matcher.RequestMatcher;
 @Order(99)
 public class OAuth2AuthorizationServerConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
-	private final OAuth2AuthorizationServerConfigurerWrapper oAuth2AuthorizationServerConfigurerWrapper;
+	private final SecurityProperties securityProperties;
+
+	private final List<OAuth2AuthorizationServerConfigurerCustomizer> oAuth2AuthorizationServerConfigurerCustomizerList;
+
+	private final CaptchaValidator captchaValidator;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		// 授权服务器配置
+		OAuth2AuthorizationServerConfigurer authorizationServerConfigurer = new OAuth2AuthorizationServerConfigurer();
+		for (OAuth2AuthorizationServerConfigurerCustomizer customizer : oAuth2AuthorizationServerConfigurerCustomizerList) {
+			customizer.customize(authorizationServerConfigurer, http);
+		}
+
+		// 授权服务器配置包装，添加验证码以及密码加解密处理
+		OAuth2AuthorizationServerConfigurerWrapper configurerWrapper = new OAuth2AuthorizationServerConfigurerWrapper(
+				authorizationServerConfigurer, securityProperties, captchaValidator);
+
 		// @formatter:off
-		RequestMatcher endpointsMatcher = oAuth2AuthorizationServerConfigurerWrapper.getEndpointsMatcher();
+		RequestMatcher endpointsMatcher = configurerWrapper.getEndpointsMatcher();
 		http.requestMatcher(endpointsMatcher)
 				.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
 				.csrf(csrf -> csrf.ignoringRequestMatchers(endpointsMatcher))
-				.apply(oAuth2AuthorizationServerConfigurerWrapper);
-
+				.apply(configurerWrapper);
 		// @formatter:off
 	}
 }
---
+++ ballcat-security/ballcat-spring-security-oauth2-authorization-server/src/main/java/org/ballcat/springsecurity/oauth2/server/authorization/configurer/OAuth2AuthorizationServerConfigurerCustomizer.java
@@ -0,0 +1,21 @@
+package org.ballcat.springsecurity.oauth2.server.authorization.configurer;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+
+/**
+ * 授权服务器配置的自定义器
+ *
+ * @author hccake
+ */
+@FunctionalInterface
+public interface OAuth2AuthorizationServerConfigurerCustomizer {
+
+	/**
+	 * 对授权服务器配置进行自定义
+	 * @param oAuth2AuthorizationServerConfigurer OAuth2AuthorizationServerConfigurer
+	 * @param httpSecurity security configuration
+	 */
+	void customize(OAuth2AuthorizationServerConfigurer oAuth2AuthorizationServerConfigurer, HttpSecurity httpSecurity);
+
+}
