2bdd507f863072c0806e5792e906d6e4cd380e81
lingting <sunlisten.gzm@gmail.com>
2022-11-10 16:58:13 +0800

    :bug: 修复 MdcTaskDecorator 清除子线程MDC上下文时机不对

--- ballcat-admin/ballcat-admin-core/src/main/java/com/hccake/ballcat/admin/upms/config/task/MdcTaskDecorator.java
+++ ballcat-admin/ballcat-admin-core/src/main/java/com/hccake/ballcat/admin/upms/config/task/MdcTaskDecorator.java
@@ -1,6 +1,7 @@
 package com.hccake.ballcat.admin.upms.config.task;
 
 import cn.hutool.core.map.MapUtil;
+import org.jboss.logging.NDC;
 import org.slf4j.MDC;
 import org.springframework.core.task.TaskDecorator;
 
@@ -15,20 +16,20 @@ public class MdcTaskDecorator implements TaskDecorator {
 
 	@Override
 	public Runnable decorate(Runnable runnable) {
-		try {
-			Map<String, String> copyOfContextMap = MDC.getCopyOfContextMap();
-			return () -> {
-				// 现在：@Async线程上下文！
-				// 恢复Web线程上下文的MDC数据
-				if (MapUtil.isNotEmpty(copyOfContextMap)) {
-					MDC.setContextMap(copyOfContextMap);
-				}
+		final Map<String, String> copyOfContextMap = MDC.getCopyOfContextMap();
+		return () -> {
+			if (MapUtil.isNotEmpty(copyOfContextMap)) {
+				// 现在：@Async线程上下文！ 恢复Web线程上下文的MDC数据
+				MDC.setContextMap(copyOfContextMap);
+			}
+
+			try {
 				runnable.run();
-			};
-		}
-		finally {
-			MDC.clear();
-		}
+			}
+			finally {
+				NDC.clear();
+			}
+		};
 	}
 
 }
