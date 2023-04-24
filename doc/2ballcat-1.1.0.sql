556f84301f88232b1ed318fd8c2b241a896b17c4
Hccake <chengbohua@foxmail.com>
2023-04-24 22:03:59 +0800

    :bookmark: release v1.1.0

--- doc/2ballcat-1.1.0.sql
+++ doc/2ballcat-1.1.0.sql
@@ -123,36 +123,6 @@ CREATE TABLE `notify_user_announcement`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 38 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户公告表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
--- Records of notify_user_announcement
--- ----------------------------
-
--- ----------------------------
--- Table structure for oauth_client_details
--- ----------------------------
-DROP TABLE IF EXISTS `oauth_client_details`;
-CREATE TABLE `oauth_client_details`  (
-  `client_id` varchar(250) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `resource_ids` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `client_secret` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `scope` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `authorized_grant_types` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `web_server_redirect_uri` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `authorities` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `access_token_validity` int(11) NULL DEFAULT NULL,
-  `refresh_token_validity` int(11) NULL DEFAULT NULL,
-  `additional_information` varchar(4096) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `autoapprove` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`client_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = 'OAuth客户端配置' ROW_FORMAT = DYNAMIC;
-
--- ----------------------------
--- Records of oauth_client_details
--- ----------------------------
-INSERT INTO `oauth_client_details` VALUES ('actuator', NULL, '$2a$10$frNrwNcb5rUeNBd7EhME6uN7zTOe0qKlDVsT2SgZZDitJXuJJ7wDO', 'server', 'password,refresh_token', NULL, NULL, NULL, NULL, NULL, 'true');
-INSERT INTO `oauth_client_details` VALUES ('test', NULL, '$2a$10$8DrIu79gvgx8.nQXuWGPR.tG/SHh547krcxhMeDrikJjUWlLua4.K', 'skip_captcha,skip_password_decode', 'password,client_credentials,refresh_token,mobile', NULL, NULL, NULL, NULL, NULL, 'true');
-INSERT INTO `oauth_client_details` VALUES ('ui', NULL, '$2a$10$8UbJyUN9kdE16RlqSx9Sc.YLIjKLDWFS2Nvev.uN/P2OrrCtVkuMO', 'server', 'password,refresh_token', NULL, NULL, NULL, NULL, NULL, 'true');
-
--- ----------------------------
 -- Table structure for sys_config
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_config`;
@@ -628,5 +598,68 @@ CREATE TABLE `sys_user_role`  (
 INSERT INTO `sys_user_role` VALUES (1, 1, 'ROLE_ADMIN');
 INSERT INTO `sys_user_role` VALUES (6, 10, 'ROLE_SALES_EXECUTIVE');
 INSERT INTO `sys_user_role` VALUES (4, 1, 'ROLE_TEST');
+
+
+/*
+IMPORTANT:
+    If using PostgreSQL, update ALL columns defined with 'blob' to 'text',
+    as PostgreSQL does not support the 'blob' data type.
+*/
+CREATE TABLE oauth2_registered_client (
+                                        id varchar(100) NOT NULL,
+                                        client_id varchar(100) NOT NULL,
+                                        client_id_issued_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+                                        client_secret varchar(200) DEFAULT NULL,
+                                        client_secret_expires_at timestamp DEFAULT NULL,
+                                        client_name varchar(200) NOT NULL,
+                                        client_authentication_methods varchar(1000) NOT NULL,
+                                        authorization_grant_types varchar(1000) NOT NULL,
+                                        redirect_uris varchar(1000) DEFAULT NULL,
+                                        scopes varchar(1000) NOT NULL,
+                                        client_settings varchar(2000) NOT NULL,
+                                        token_settings varchar(2000) NOT NULL,
+                                        PRIMARY KEY (id)
+);
+
+CREATE TABLE oauth2_authorization (
+                                    id varchar(100) NOT NULL,
+                                    registered_client_id varchar(100) NOT NULL,
+                                    principal_name varchar(200) NOT NULL,
+                                    authorization_grant_type varchar(100) NOT NULL,
+                                    authorized_scopes varchar(1000) DEFAULT NULL,
+                                    attributes blob DEFAULT NULL,
+                                    state varchar(500) DEFAULT NULL,
+                                    authorization_code_value blob DEFAULT NULL,
+                                    authorization_code_issued_at timestamp DEFAULT NULL,
+                                    authorization_code_expires_at timestamp DEFAULT NULL,
+                                    authorization_code_metadata blob DEFAULT NULL,
+                                    access_token_value blob DEFAULT NULL,
+                                    access_token_issued_at timestamp DEFAULT NULL,
+                                    access_token_expires_at timestamp DEFAULT NULL,
+                                    access_token_metadata blob DEFAULT NULL,
+                                    access_token_type varchar(100) DEFAULT NULL,
+                                    access_token_scopes varchar(1000) DEFAULT NULL,
+                                    oidc_id_token_value blob DEFAULT NULL,
+                                    oidc_id_token_issued_at timestamp DEFAULT NULL,
+                                    oidc_id_token_expires_at timestamp DEFAULT NULL,
+                                    oidc_id_token_metadata blob DEFAULT NULL,
+                                    refresh_token_value blob DEFAULT NULL,
+                                    refresh_token_issued_at timestamp DEFAULT NULL,
+                                    refresh_token_expires_at timestamp DEFAULT NULL,
+                                    refresh_token_metadata blob DEFAULT NULL,
+                                    PRIMARY KEY (id)
+);
+
+CREATE TABLE oauth2_authorization_consent (
+                                            registered_client_id varchar(100) NOT NULL,
+                                            principal_name varchar(200) NOT NULL,
+                                            authorities varchar(1000) NOT NULL,
+                                            PRIMARY KEY (registered_client_id, principal_name)
+);
+
+
+INSERT INTO `oauth2_registered_client` (`id`, `client_id`, `client_id_issued_at`, `client_secret`, `client_secret_expires_at`, `client_name`, `client_authentication_methods`, `authorization_grant_types`, `redirect_uris`, `scopes`, `client_settings`, `token_settings`) VALUES ('2e646465-c515-4933-8c55-977502d22878', 'ui', '2023-03-30 10:28:24', '{noop}ui', NULL, '2e646465-c515-4933-8c55-977502d22878', 'client_secret_basic', 'refresh_token,client_credentials,password,authorization_code', 'http://127.0.0.1:8080/authorized', '', '{\"@class\":\"java.util.Collections$UnmodifiableMap\",\"settings.client.require-proof-key\":false,\"settings.client.require-authorization-consent\":true}', '{\"@class\":\"java.util.Collections$UnmodifiableMap\",\"settings.token.reuse-refresh-tokens\":true,\"settings.token.id-token-signature-algorithm\":[\"org.springframework.security.oauth2.jose.jws.SignatureAlgorithm\",\"RS256\"],\"settings.token.access-token-time-to-live\":[\"java.time.Duration\",86400.000000000],\"settings.token.access-token-format\":{\"@class\":\"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat\",\"value\":\"reference\"},\"settings.token.refresh-token-time-to-live\":[\"java.time.Duration\",259200.000000000],\"settings.token.authorization-code-time-to-live\":[\"java.time.Duration\",300.000000000]}');
+INSERT INTO `oauth2_registered_client` (`id`, `client_id`, `client_id_issued_at`, `client_secret`, `client_secret_expires_at`, `client_name`, `client_authentication_methods`, `authorization_grant_types`, `redirect_uris`, `scopes`, `client_settings`, `token_settings`) VALUES ('ca4ab106-6435-4c32-b2b8-1afd83d22c25', 'test', '2023-03-30 10:28:23', '{noop}test', NULL, 'ca4ab106-6435-4c32-b2b8-1afd83d22c25', 'client_secret_basic', 'refresh_token,client_credentials,password,authorization_code', 'http://127.0.0.1:8080/authorized', 'skip_captcha,skip_password_decode', '{\"@class\":\"java.util.Collections$UnmodifiableMap\",\"settings.client.require-proof-key\":false,\"settings.client.require-authorization-consent\":true}', '{\"@class\":\"java.util.Collections$UnmodifiableMap\",\"settings.token.reuse-refresh-tokens\":true,\"settings.token.id-token-signature-algorithm\":[\"org.springframework.security.oauth2.jose.jws.SignatureAlgorithm\",\"RS256\"],\"settings.token.access-token-time-to-live\":[\"java.time.Duration\",86400.000000000],\"settings.token.access-token-format\":{\"@class\":\"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat\",\"value\":\"reference\"},\"settings.token.refresh-token-time-to-live\":[\"java.time.Duration\",259200.000000000],\"settings.token.authorization-code-time-to-live\":[\"java.time.Duration\",300.000000000]}');
+
 
 SET FOREIGN_KEY_CHECKS = 1;
