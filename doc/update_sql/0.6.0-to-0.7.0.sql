2b6fd5bd2695ebf7f3aad29bab6b6c294a3f1267
Hccake <chengbohua@foxmail.com>
2023-05-25 23:17:29 +0800

    :truck: 业务模块相关 SQL 文件迁移至新的仓库：ballcat-business
    https://github.com/ballcat-projects/ballcat-business

--- doc/update_sql/0.6.0-to-0.7.0.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `log_operation_log`
-ADD COLUMN `result` text COMMENT '操作结果' AFTER `params`;
--- doc/update_sql/0.7.0-to-1.0.0.sql
+++
@@ -1,2 +0,0 @@
--- 修改测试客户端的 scope, 用于跳过验证码和密码解密
-update oauth_client_details set scope = 'skip_captcha,skip_password_decode' where client_id = 'test'
