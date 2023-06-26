5a5610e0a3eb3bfac6a153e523f4cb3a6814b4ed
lingting <sunlisten.gzm@gmail.com>
2023-06-26 19:29:37 +0800

    :zap: 修改 undertow servlet配置自定义bean注册方式, 方便外部重写.

--- web/ballcat-spring-boot-starter-web/src/main/java/org/ballcat/autoconfigure/web/undertow/BallcatUndertowAutoConfiguration.java
+++ web/ballcat-spring-boot-starter-web/src/main/java/org/ballcat/autoconfigure/web/undertow/BallcatUndertowAutoConfiguration.java
@@ -1,15 +1,11 @@
 package org.ballcat.autoconfigure.web.undertow;
 
-import io.undertow.server.DefaultByteBufferPool;
 import io.undertow.servlet.api.Deployment;
 import io.undertow.servlet.api.DeploymentInfo;
 import io.undertow.servlet.spec.ServletContextImpl;
-import io.undertow.websockets.jsr.WebSocketDeploymentInfo;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.util.StringUtils;
 
@@ -26,13 +22,8 @@ public class BallcatUndertowAutoConfiguration {
 
 	@Bean
 	@ConditionalOnMissingBean
-	public WebServerFactoryCustomizer<UndertowServletWebServerFactory> undertowServletWebServerFactoryCustomization() {
-		return factory -> factory.addDeploymentInfoCustomizers(deploymentInfo -> {
-			WebSocketDeploymentInfo webSocketDeploymentInfo = new WebSocketDeploymentInfo();
-			webSocketDeploymentInfo.setBuffers(new DefaultByteBufferPool(false, 4096, -1, 24, 0));
-			deploymentInfo.addServletContextAttribute("io.undertow.websockets.jsr.WebSocketDeploymentInfo",
-					webSocketDeploymentInfo);
-		});
+	public BallcatUndertowServletWebServerFactory undertowServletWebServerFactoryCustomization() {
+		return new BallcatUndertowServletWebServerFactory();
 	}
 
 	/**
