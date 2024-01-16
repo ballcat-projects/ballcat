bdfea440f32bc9f3e600b600826542f4d0d8ded5
lingting <sunlisten.gzm@gmail.com>
2024-01-16 10:26:25 +0800

    :sparkles: 完善ThreadPool方法

--- common/ballcat-common-util/src/main/java/org/ballcat/common/thread/ThreadPool.java
+++ common/ballcat-common-util/src/main/java/org/ballcat/common/thread/ThreadPool.java
@@ -1,51 +1,43 @@
-/*
- * Copyright 2023 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.ballcat.common.thread;
 
-import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.ballcat.common.function.ThrowingRunnable;
+import org.slf4j.MDC;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 
 /**
  * @author lingting 2022/11/17 20:15
  */
 @Slf4j
+@Getter
+@AllArgsConstructor
 @SuppressWarnings("java:S6548")
-@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class ThreadPool {
 
-	protected static final ThreadPool INSTANCE;
+	protected static final ThreadPool THREAD_POOL;
 
-	protected static final Integer QUEUE_MAX = 100;
+	protected static final Integer QUEUE_MAX = 10;
 
-	@Getter
-	protected final ThreadPoolExecutor pool;
+	protected ThreadPoolExecutor pool;
 
 	static {
-		INSTANCE = new ThreadPool(new ThreadPoolExecutor(
+		THREAD_POOL = new ThreadPool(new ThreadPoolExecutor(
 				// 核心线程数大小. 不论是否空闲都存在的线程
 				300,
-				// 最大线程数 - 10万个
-				100000,
+				// 最大线程数 - 1万个
+				10000,
 				// 存活时间. 非核心线程数如果空闲指定时间. 就回收
 				// 存活时间不宜过长. 避免任务量遇到尖峰情况时. 大量空闲线程占用资源
 				10,
@@ -61,47 +53,110 @@ public class ThreadPool {
 	}
 
 	public static ThreadPool instance() {
-		return INSTANCE;
+		return THREAD_POOL;
+	}
+
+	public static ThreadPool update(ThreadPoolExecutor executor) {
+		ThreadPool instance = instance();
+		instance.pool = executor;
+		return instance;
 	}
 
 	/**
-	 * 线程池是否活跃
+	 * 线程池是否运行中
 	 */
 	public boolean isRunning() {
-		return getCount() > 0;
+		ThreadPoolExecutor executor = getPool();
+		return !executor.isShutdown() && !executor.isTerminated() && !executor.isTerminating();
 	}
 
 	/**
-	 * 线程当前活跃数量
+	 * 核心线程数
 	 */
-	public long getCount() {
+	public long getCorePoolSize() {
+		return getPool().getCorePoolSize();
+	}
+
+	/**
+	 * 活跃线程数
+	 */
+	public long getActiveCount() {
+		return getPool().getActiveCount();
+	}
+
+	/**
+	 * 已执行任务总数
+	 */
+	public long getTaskCount() {
 		return getPool().getTaskCount();
 	}
 
-	public void execute(Runnable runnable) {
+	/**
+	 * 允许的最大线程数量
+	 */
+	public long getMaximumPoolSize() {
+		return getPool().getMaximumPoolSize();
+	}
+
+	/**
+	 * 是否可能触发拒绝策略, 仅为估算
+	 */
+	public boolean isReject() {
+		long activeCount = getActiveCount();
+		long size = getMaximumPoolSize();
+
+		// 活跃线程占比未达到 90% 不可能
+		long per = activeCount / size;
+		if (per <= 90) {
+			return false;
+		}
+
+		// 占比达到90%的情况下, 剩余可用线程数小于10 则可能触发拒绝
+		return size - activeCount < 10;
+	}
+
+	public void execute(ThrowingRunnable runnable) {
 		execute(null, runnable);
 	}
 
-	public void execute(String name, Runnable runnable) {
+	public void execute(String name, ThrowingRunnable runnable) {
+		// 获取当前线程的配置
+		Map<String, String> map = MDC.getCopyOfContextMap();
 		getPool().execute(() -> {
 			Thread thread = Thread.currentThread();
 			String oldName = thread.getName();
 			if (StringUtils.hasText(name)) {
 				thread.setName(name);
 			}
+
+			// 存在则填充
+			if (!CollectionUtils.isEmpty(map)) {
+				MDC.setContextMap(map);
+			}
+
 			try {
 				runnable.run();
 			}
+			catch (InterruptedException e) {
+				thread.interrupt();
+				log.warn("线程池内部线程被中断!");
+			}
 			catch (Throwable throwable) {
-				log.error("线程发生异常!", throwable);
-				if (!(throwable instanceof Exception)) {
-					throw throwable;
-				}
+				log.error("线程池内部线程异常!", throwable);
 			}
 			finally {
 				thread.setName(oldName);
+				MDC.clear();
 			}
 		});
+	}
+
+	public <T> CompletableFuture<T> async(Supplier<T> supplier) {
+		return CompletableFuture.supplyAsync(supplier, pool);
+	}
+
+	public <T> Future<T> submit(Callable<T> callable) {
+		return pool.submit(callable);
 	}
 
 }
